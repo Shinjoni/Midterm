13edb6d446699bf20b0838146c9f987a474c5cbc
Shinjoni <Shinjoni@gmail.com>
2017-10-19 00:47:45 -0400

    Scripts at least
    PC script please

---
+++ Midterm-Chai/Assets/inClassScript/Level1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1 : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+
+		SceneManager.LoadScene ("level 1");
+
+
+	}
+}
